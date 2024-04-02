7d59dbc95b27c4bf0a6e3c61cd53c857d0b6f78c
jysophia <jysophiakim@gmail.com>
2024-04-02 04:02:34 -0700

    displays analysis when button is clicked

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -4,40 +4,43 @@ import FileInput from "./components/FileInput.jsx";
 import AnalysisContainer from "./components/AnalysisContainer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const [data, setData] = useState([
-    {
-      "key": 0,
-      "name": "main",
-      "start": 0,
-      "end": 4000
-    },
-    {
-      "key": 1,
-      "name": "testFunc",
-      "start": 100,
-      "end": 3900
-    },
-    {
-      "key": 2,
-      "name": "func1",
-      "start": 400,
-      "end": 1000
-    },
-    {
-      "key": 3,
-      "name": "func2",
-      "start": 1100,
-      "end": 1900
-    }
-  ])
+    const [showAnalysis, setShowAnalysis] = useState(false)
+    const [data, setData] = useState([
+        {
+            "key": 0,
+            "name": "main",
+            "start": 0,
+            "end": 4000
+        },
+        {
+            "key": 1,
+            "name": "testFunc",
+            "start": 100,
+            "end": 3900
+        },
+        {
+            "key": 2,
+            "name": "func1",
+            "start": 400,
+            "end": 1000
+        },
+        {
+            "key": 3,
+            "name": "func2",
+            "start": 1100,
+            "end": 1900
+        }
+    ])
 
   return (
     <>
-        <FileInput/>
-        <div className="analysis-wrapper">
-          <AnalysisContainer data={data}/>
+        <FileInput setShowAnalysis={setShowAnalysis}/>
+        <div>
+            {showAnalysis &&
+                <div className="analysis-wrapper">
+                    <AnalysisContainer data={data}/>
+                </div>
+            }
         </div>
     </>
   )
--- ui/src/components/FileInput.jsx
+++ ui/src/components/FileInput.jsx
@@ -1,10 +1,17 @@
 import 'react'
+import {useState} from "react";
+import AnalysisContainer from "./AnalysisContainer.jsx";
 
-const FileInput = () => {
+const FileInput = ({setShowAnalysis}) => {
+
+    const analyzeCode = () => {
+        setShowAnalysis(true);
+    }
+
     return (
         <div className="file-input">
             <textarea className="textarea textarea-lg textarea-primary" placeholder="Insert your code here"></textarea>
-            <button className="btn btn-active btn-primary btn-setting">Generate</button>
+            <button className="btn btn-active btn-primary btn-setting" onClick={analyzeCode}>Analyze</button>
         </div>
     )
 }
