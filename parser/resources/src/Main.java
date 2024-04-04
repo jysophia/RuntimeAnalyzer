ed0efb57181e5ea5c2616949927c0b29a3637acb
daveborrel <dborrel@student.ubc.ca>
2024-04-04 16:20:03 -0700

    updated main

--- parser/resources/src/Main.java
+++ parser/resources/src/Main.java
@@ -1,10 +1,11 @@
 import TestProgramPackage.AnotherTestClass;
-
 public class Main {
   public static void main(String[] args) {
     TestProgramClass tc = new TestProgramClass(0);
-    AnotherTestClass anotherTestClass = new AnotherTestClass();
+    AnotherTestClass atc = new AnotherTestClass();
     printSomething(tc, 15);
+    exponentiallyIncreaseValue(tc, 5);
+    divideUntilFraction(tc);
   }
 
   public static void printSomething(TestProgramClass tpc, int val) {
@@ -12,6 +13,23 @@ public class Main {
     tpc.returnTest();
     tpc.setVal(val);
     tpc.returnTest();
+    System.out.println("The new value of the TestProgramClass is " + tpc.getVal());
+  }
+
+  public static void exponentiallyIncreaseValue(TestProgramClass tpc, int val) {
+    for (int i = 0; i < val; i++) {
+      tpc.val = tpc.val * tpc.val;
+    }
+    System.out.println("The new value of the TestProgramClass is " + tpc.getVal());
+  }
+
+  public static void divideUntilFraction(TestProgramClass tpc) {
+    tpc.val = tpc.val / 2;
+
+    while (tpc.val > 0) {
+      divideUntilFraction(tpc);
+    }
+
     System.out.println("The new value of the TestProgramClass is " + tpc.getVal());
   }
 }
