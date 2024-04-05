2a31ed2e14608a70056237dcdc5c8bcf704654c8
jysophia <jysophiakim@gmail.com>
2024-04-05 01:33:15 -0700

    first draft of uploading multiple files and saving as variables

--- ui/src/components/FileInput.jsx
+++ ui/src/components/FileInput.jsx
@@ -1,54 +1,33 @@
-import 'react'
+import React, {useState} from 'react'
 
 function FileInput({setShowAnalysis}) {
-    let input = ""
-    let argInput = ""
+    const [files, setFiles] = useState([]);
+    const [newLines, setNewLines] = useState([]);
 
-    const parseArgs = (argInput) => {
-        let args = []
-        let arg = ""
-        for (let i = 0; i < argInput.length; i++) {
-            if (argInput[i] !== " ") {
-                arg += argInput[i];
-                if (i === argInput.length - 1) {
-                    args.push(arg);
-                    arg = "";
-                }
-            } else {
-                args.push(arg);
-                arg = "";
-            }
-        }
-        return args;
+    const uploadAndAddAnotherFile = () => {
+        setNewLines([...newLines, newLines.length]);
     }
 
-    const handleArgs = (event) => {
-        argInput = event.target.value;
+    const handleUpload = (event) => {
+        setFiles([...files, event.target.files[0]]);
     }
 
-    const handleCode = (event) => {
-        input = event.target.value
-    }
-
-    const handleUpload = (code, args) => {
-        // send to backend?
-        console.log(input);
-        console.log(args);
-    }
-
-    const analyzeCode = () => {
+    const analyzeCode = (event) => {
         setShowAnalysis(true);
-        let argArray = parseArgs(argInput);
-        handleUpload(input, argArray);
     }
 
     return (
         <div className="file-upload">
           <div className="text-btn-container">
-              <div>
-                  <input type="text" placeholder="Insert code here" className="input input-bordered input-primary w-full max-w-xs" onChange={handleCode} />
-                  <input type="text" placeholder="Insert arguments here" className="input input-bordered input-primary w-full max-w-xs" onChange={handleArgs} />
+              <div className="file-upload-container">
+                  <input type="file" className="file-input w-full max-w-xs" onChange={handleUpload} />
+                  <button className="btn btn-active btn-primary btn-add-setting" onClick={uploadAndAddAnotherFile}>+</button>
               </div>
+              {
+                  newLines.map(() => (
+                      <input type="file" className="file-input w-full max-w-xs" onChange={handleUpload} />
+                  ))
+              }
             <div className="btn-container">
               <button className="btn btn-active btn-primary btn-setting" onClick={analyzeCode}>Analyze</button>
             </div>
