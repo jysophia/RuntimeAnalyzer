1d0db848fdb5754550494567e27c4e4006b5f352
William <wgumboc@gmail.com>
2024-04-05 01:52:00 -0700

    Style changes

--- ui/src/components/FileInput.jsx
+++ ui/src/components/FileInput.jsx
@@ -20,14 +20,18 @@ function FileInput({setShowAnalysis}) {
         <div className="file-upload">
           <div className="text-btn-container">
               <div className="file-upload-container">
-                  <input type="file" className="file-input w-full max-w-xs" onChange={handleUpload} />
-                  <button className="btn btn-active btn-primary btn-add-setting" onClick={addAnotherFile}>+</button>
-              </div>
-              {
-                  newLines.map(() => (
+                  <button className="btn btn-active btn-primary btn-add-setting" onClick={addAnotherFile}>Add Another JSON</button>
+                  <div className="file-input-container">
                       <input type="file" className="file-input w-full max-w-xs" onChange={handleUpload} />
-                  ))
-              }
+                  </div>
+                  {
+                      newLines.map(() => (
+                          <div className="file-input-container">
+                              <input type="file" className="file-input w-full max-w-xs" onChange={handleUpload} />
+                          </div>
+                      ))
+                  }
+              </div>
             <div className="btn-container">
               <button className="btn btn-active btn-primary btn-setting" onClick={analyzeCode}>Analyze</button>
             </div>
